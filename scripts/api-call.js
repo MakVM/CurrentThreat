7325d3c32170413527ebbb176fdb88e4b56937c0
MakVM <101976709+MakVM@users.noreply.github.com>
2025-11-18 22:01:36 +0300

    Update api-call.js

--- scripts/api-call.js
+++ scripts/api-call.js
@@ -43,8 +43,8 @@ async function fetchData() {
     },
     body: JSON.stringify({
       // 🔧 CHANGE THESE FOR YOUR USE CASE:
-      entities: "Crypto.com, PayPal, Revolut, Binance, Coinbase",    // ← What to track
-      topic: "attack, hack, exploit",               // ← What topic 
+      entities: "finance",    // ← What to track
+      topic: "attack, hack, exploit, scam, fraud, breach, compromise, leak, vulnerability",               // ← What topic 
       startTime,
       endTime
     }),
