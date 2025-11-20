9f3f09e7fa26b99e56bd025c19b9f6d9bf1f1be8
MakVM <101976709+MakVM@users.noreply.github.com>
2025-11-20 18:25:34 +0300

    Update vite.spa.config.ts

--- vite.spa.config.ts
+++ vite.spa.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: ".",         // <-- point to your React app folder
-  base: "/website-demo3/",
+  base: "/CurrentThreat/",
   plugins: [react()],
   build: {
     outDir: "dist-spa",    // relative to root
